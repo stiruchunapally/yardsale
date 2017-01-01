757a170154612492537afac034af25215e4395e5
Srinivas Raj <srinivas@Srinivass-MacBook-Pro.local>
2017-01-01 00:26:54 -0700

    initial commit

---
+++ app/src/main/java/com/sreekar/yardsale/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sreekar.yardsale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
