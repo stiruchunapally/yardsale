5efdbd894a84b1c717912a8d397104c69faf1c35
Srinivas Raj <srinivas@Srinivass-MacBook-Pro.local>
2017-01-02 02:13:03 -0700

    add items to firebase database

--- app/src/main/java/com/sreekar/yardsale/DonateActivity.java
+++ app/src/main/java/com/sreekar/yardsale/DonateActivity.java
@@ -2,23 +2,36 @@ package com.sreekar.yardsale;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.RatingBar;
 
-public class DonateActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.sreekar.yardsale.models.Item;
 
-    private ImageButton buploadImage;
-    private EditText etname;
-    private EditText etprice;
-    private EditText etdescription;
-    private TextView tvcondition;
-    private ImageView mImageLabel;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DonateActivity extends BaseActivity implements View.OnClickListener {
+
+    private DatabaseReference mDatabase;
+
+    private ImageButton btnCaptureImage;
+    private Button btnSubmit;
+    private EditText title;
+    private ImageView itemImage;
+    private RatingBar ratingCondition;
+    private EditText suggestedPrice;
+    private EditText description;
 
     private final int REQUEST_IMAGE_CAPTURE = 11;
 
@@ -27,16 +40,20 @@ public class DonateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
 
-        buploadImage = (ImageButton) findViewById(R.id.upload_image);
-        mImageLabel = (ImageView) findViewById(R.id.itemImageView);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        buploadImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        });
+        // Views
+        btnCaptureImage = (ImageButton) findViewById(R.id.button_capture_image);
+        btnSubmit = (Button) findViewById(R.id.button_submit);
+        itemImage = (ImageView) findViewById(R.id.item_image);
+        title = (EditText) findViewById(R.id.text_item_title);
+        ratingCondition = (RatingBar) findViewById(R.id.rating_condition);
+        suggestedPrice = (EditText) findViewById(R.id.text_price);
+        description = (EditText) findViewById(R.id.text_description);
+
+        // Click listeners
+        btnSubmit.setOnClickListener(this);
+        btnCaptureImage.setOnClickListener(this);
     }
 
     @Override
@@ -44,10 +61,53 @@ public class DonateActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageLabel.setImageBitmap(imageBitmap);
-
-//            BitmapDrawable bitmapDrawable = ((BitmapDrawable) mImageLabel.getDrawable());
-//            Bitmap bitmap = bitmapDrawable .getBitmap();
+            itemImage.setImageBitmap(imageBitmap);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.button_capture_image) {
+            captureImage();
+        } else if (i == R.id.button_submit) {
+            submit();
+        }
+    }
+
+    private void captureImage() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    private void submit() {
+        BitmapDrawable bitmapDrawable = ((BitmapDrawable) itemImage.getDrawable());
+        Bitmap bitmap = bitmapDrawable .getBitmap();
+        String userId = getUid();
+
+        Item item = new Item();
+        item.setCondition(ratingCondition.getRating());
+        item.setDescription(description.getText().toString());
+        item.setImage(encodeBitmap(bitmap));
+        item.setPrice(Float.valueOf(suggestedPrice.getText().toString()));
+        item.setSellerUserId(userId);
+        item.setTitle(title.getText().toString());
+
+        String key = mDatabase.child("items").push().getKey();
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/items/" + key, item);
+        childUpdates.put("/user-items/" + userId + "/" + key, item);
+
+        mDatabase.updateChildren(childUpdates);
+
+        // Go to MainActivity
+        startActivity(new Intent(DonateActivity.this, MainActivity.class));
+        finish();
+    }
+
+    private String encodeBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        return Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
     }
 }
---
+++ app/src/main/java/com/sreekar/yardsale/models/Item.java
@@ -0,0 +1,58 @@
+package com.sreekar.yardsale.models;
+
+public class Item {
+    private String sellerUserId;
+    private String title;
+    private String description;
+    private Float price;
+    private Float condition;
+    private String image;
+
+    public String getSellerUserId() {
+        return sellerUserId;
+    }
+
+    public void setSellerUserId(String sellerUserId) {
+        this.sellerUserId = sellerUserId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Float getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Float condition) {
+        this.condition = condition;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
